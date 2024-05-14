c24ef75c8160b601e96101d770a6b677a38f7b9c
saniyaj mallik <saniyajmallik12@gmail.com>
2024-05-14 12:59:15 +0530

    deploy2

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 // db connection
 export const connectDB = () => {
-
+console.log(process.env.MONGO_URL);
     mongoose.connect(process.env.MONGO_URL, {
 
     }).then(() => {
